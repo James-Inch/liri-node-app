5ed79b75358d478411735dabf47ecb0d29989e62
james inch <james.inch0103@gmail.com>
2018-09-08 19:34:21 -0400

    update

--- liri.js
+++ liri.js
@@ -6,56 +6,50 @@ require("dotenv").config();
 
 var request = require("request");
 
-switch (process.argv[2]) {
-  case "movie-this":
-    movieThis();
-    break;
-  case "concert-this":
-    concertThis();
-    break;
+var fs = require("fs");
+
+var inportantStuff = process.argv.slice(2)
+
+start(inportantStuff);
+
+function start(cmdArr) {
+  switch (cmdArr[0]) {
+    case "movie-this":
+      movieThis(cmdArr.slice(1).join(" "));
+      break;
+    case "concert-this":
+      concertThis();
+      break;
+  }
 }
-
 // var spotify = new Spotify(keys.spotify);
-function movieThis() {
-  var nodeArgs = process.argv;
-
-  var movieName = "";
-
-  for (i = 3; i < nodeArgs.length; i++) {
-    if (i > 3 && i < nodeArgs.length) {
-      movieName = movieName + "+" + nodeArgs[i];
-    } else {
-      movieName += nodeArgs[i];
-    }
-  };
+function movieThis(movieName) {
 
   var url = ("http://www.omdbapi.com/?t=" + movieName + "&apikey=4c9d8e40");
 
   request(url, function (error, response, body) {
 
     if (!error && response.statusCode === 200) {
-      var title = (JSON.parse(body).Title);
-      var year = (JSON.parse(body).Year);
-      var imdbRating = (JSON.parse(body).Ratings[0]["Value"]);
-      var rtRating = (JSON.parse(body).Ratings[1]["Value"]);
-      var country = (JSON.parse(body).Country);
-      var language = (JSON.parse(body).Language);
-      var actors = (JSON.parse(body).Actors);
-      var plot = (JSON.parse(body).Plot);
 
-      console.log(
-        `
-      Title: ${title}
-      Year: ${year}  
+      var data = JSON.parse(body);
+    
+      var imdbRating = (data.Ratings[0]["Value"]);
+
+      var rtRating = (data.Ratings[1]["Value"]);
+
+      var formattedMovieInfo = (`
+      Title: ${data.Title}
+      Year: ${data.Year}  
       IMDB Rating: ${imdbRating}
       Rotten Tomatoes Rating: ${rtRating}
-      Produced in: ${country}
-      You can watch it in: ${language}
-      Actors: ${actors}
-      \n
-      Plot: ${plot}
+      Produced in: ${data.Country}
+      You can watch it in: ${data.Language}
+      Actors: ${data.Actors}
+
+      Plot: ${data.Plot}
       `
       );
+      console.log(formattedMovieInfo);
     }
 
   });
@@ -78,9 +72,14 @@ function concertThis() {
 
   request(url, function (error, response, body) {
 
-    if (!error && response.statusCode === 200){
+    if (!error && response.statusCode === 200) {
       var response = (response);
-      console.log(JSON.parse(body).venue[0]);
+    var data = JSON.parse(body)[0].venue;
+    console.log(data);
     };
   });
 };
+
+function doWhatItSays() {
+
+}
